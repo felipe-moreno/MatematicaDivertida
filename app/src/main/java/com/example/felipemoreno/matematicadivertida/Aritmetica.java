0885358599c0c4fe701f2abba10c625e9c3b8843
felipe-moreno <felipemorenolopes@gmail.com>
2018-04-29 17:29:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/felipemoreno/matematicadivertida/Aritmetica.java
@@ -0,0 +1,13 @@
+package com.example.felipemoreno.matematicadivertida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aritmetica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aritmetica);
+    }
+}
---
+++ app/src/main/java/com/example/felipemoreno/matematicadivertida/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.felipemoreno.matematicadivertida;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnContagem, btnAritmetica, btnMaiorNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnContagem = findViewById(R.id.btnContagem);
+        btnAritmetica = findViewById(R.id.btnAritmetica);
+        btnMaiorNum = findViewById(R.id.btnMaiorNum);
+
+    }
+
+    public void aritmetica (View view){
+        Intent it = new Intent(this, Aritmetica.class);
+        startActivity(it);
+    }
+}
