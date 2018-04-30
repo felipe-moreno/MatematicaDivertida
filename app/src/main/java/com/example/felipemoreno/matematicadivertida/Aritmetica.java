e5e13b5800695a72553d2d9b26ab6105b7b1877c
felipe-moreno <felipemorenolopes@gmail.com>
2018-04-30 00:52:36 -0300

    Aritmética básica - funcionando. Falta: - Mostrar resultado do último clique - Desabilitar botão

--- app/src/main/java/com/example/felipemoreno/matematicadivertida/Aritmetica.java
+++ app/src/main/java/com/example/felipemoreno/matematicadivertida/Aritmetica.java
@@ -1,9 +1,13 @@
 package com.example.felipemoreno.matematicadivertida;
 
+import android.app.IntentService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -12,8 +16,9 @@ public class Aritmetica extends AppCompatActivity {
 
     EditText etNum1, etNum2, etOperacao, etResultado;
     Button btnVerifica;
-    final int min = 0;
-    final int max = 9;
+    TextView tvContador;
+    ArrayList<Integer> dados;
+    int contador = 1, acertos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +30,64 @@ public class Aritmetica extends AppCompatActivity {
         etOperacao = findViewById(R.id.etOperacao);
         etResultado = findViewById(R.id.etResultado);
         btnVerifica = findViewById(R.id.btnVerifica);
+        tvContador = findViewById(R.id.tvContador);
 
-        int dados[] = gerarConta();
-        etNum2.setText(dados[1]);
-        etNum2.setText(dados[2]);
-        etOperacao.setText(dados[3]);
+        montaTela();
     }
 
-    public int[] gerarConta(){
+    public ArrayList<Integer> geraConta(){
         Random r = new Random();
-        int random1 = r.nextInt(10 - 0);
-        int random2 = r.nextInt(10 - 0);
+        int random1 = r.nextInt(10);
+        int random2 = r.nextInt(10);
         int randOpe = r.nextInt(3 - 1);
-        ArrayList<Integer> dados = new ArrayList<>();
-        dados[1] = random1;
-        return dados;
+        ArrayList<Integer> valores = new ArrayList<>();
+        valores.add(random1);
+        valores.add(random2);
+        valores.add(randOpe);
+        return valores;
+    }
+
+    public int fazConta(int num1, int num2, int ope){
+        int resultado = 0;
+        if (ope == 0){
+            resultado = num1 + num2;
+        } else if (ope == 1){
+            resultado = num1 - num2;
+        }
+        return resultado;
+    }
+
+    public void montaTela(){
+        dados = geraConta();
+        if (dados.get(2) == 0){
+            etOperacao.setText("+");
+        } else if (dados.get(2) == 1){
+            etOperacao.setText("-");
+        }
+        etNum1.setText(dados.get(0).toString());
+        etNum2.setText(dados.get(1).toString());
+        tvContador.setText(String.valueOf(contador));
+    }
+
+    public void verificaConta(View view){
+        if (contador == 5){
+                Toast.makeText(this, "Nota: " + acertos, Toast.LENGTH_SHORT).show();
+                btnVerifica.setClickable(false);
+        } else {
+            if (etResultado.length() == 0){
+                Toast.makeText(this, "Digite o resultado", Toast.LENGTH_SHORT).show();
+            } else {
+                int digitado = Integer.parseInt(etResultado.getText().toString());
+                int resultado = fazConta(dados.get(0), dados.get(1), dados.get(2));
+                if (digitado == resultado){
+                    Toast.makeText(this, "Acertou", Toast.LENGTH_SHORT).show();
+                    acertos++;
+                } else {
+                    Toast.makeText(this, "Errou, a resposta certa era " + resultado, Toast.LENGTH_SHORT).show();
+                }
+                contador++;
+                montaTela();
+            }
+        }
     }
 }
