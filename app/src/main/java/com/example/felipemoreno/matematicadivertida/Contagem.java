61e350f9adb22f01e8471cdab5fdd9fb6bf4f2e3
felipe-moreno <felipemorenolopes@gmail.com>
2018-05-10 21:51:41 -0300

    Aritmética básica - funcionando. Falta arrumar o final Maior número - funcionando. Falta arrumar o final Contagem - funcionando mais ou menos. Falta arrumar 2 coisas, o valor que retorna do botão e o final.

---
+++ app/src/main/java/com/example/felipemoreno/matematicadivertida/Contagem.java
@@ -0,0 +1,166 @@
+package com.example.felipemoreno.matematicadivertida;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Contagem extends AppCompatActivity {
+
+    ImageView imgNumero;
+    Button btn1, btn2, btn3;
+    ArrayList<Integer> dados;
+    int contador = 0, acertos, clickado;
+    Integer[] imagens = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,
+            R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9,
+            R.drawable.img10};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contagem);
+
+        imgNumero = findViewById(R.id.imgNumero);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+        montaTela();
+    }
+
+    public ArrayList<Integer> geraImagem() {
+        Random r = new Random();
+        int randomImage = r.nextInt(10);
+        int random1 = r.nextInt(10);
+        while (random1 == randomImage){
+            random1 = r.nextInt(10);
+        }
+        int random2 = r.nextInt(10);
+        while (random2 == randomImage || random2 == random1){
+            random2 = r.nextInt(10);
+        }
+        ArrayList<Integer> valores = new ArrayList<>();
+        valores.add(randomImage);
+        valores.add(random1);
+        valores.add(random2);
+        return valores;
+    }
+
+    public void montaTela() {
+        dados = geraImagem();
+        imgNumero.setImageResource(imagens[dados.get(0)]);
+        Random r = new Random();
+        int random = r.nextInt(3);
+        switch (random){
+            case 0:
+                btn1.setText(String.valueOf(dados.get(0) + 1));
+                btn2.setText(String.valueOf(dados.get(1) + 1));
+                btn3.setText(String.valueOf(dados.get(2) + 1));
+                break;
+            case 1:
+                btn1.setText(String.valueOf(dados.get(1) + 1));
+                btn2.setText(String.valueOf(dados.get(0) + 1));
+                btn3.setText(String.valueOf(dados.get(2) + 1));
+                break;
+            case 2:
+                btn1.setText(String.valueOf(dados.get(2) + 1));
+                btn2.setText(String.valueOf(dados.get(1) + 1));
+                btn3.setText(String.valueOf(dados.get(0) + 1));
+                break;
+        }
+    }
+
+    public void verificaNumero(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        if (contador < 5) {
+                contador++;
+                switch (view.getId()){
+                    case R.id.btn1:
+                        clickado = Integer.parseInt(btn1.getText().toString());
+                        if (clickado == (dados.get(0))) {
+                            builder.setMessage("Acertou").setTitle("Questão " + contador);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                            acertos++;
+                        } else {
+                            builder.setMessage("Errou, a resposta certa era " + (dados.get(0) + 1)).setTitle("Questão " + contador);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        }
+                        clickado = 0;
+                        break;
+                    case R.id.btn2:
+                        clickado = Integer.parseInt(btn1.getText().toString());
+                        if (clickado == (dados.get(0))) {
+                            builder.setMessage("Acertou").setTitle("Questão " + contador);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                            acertos++;
+                        } else {
+                            builder.setMessage("Errou, a resposta certa era " + (dados.get(0) + 1)).setTitle("Questão " + contador);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        }
+                        clickado = 0;
+                        break;
+                    case R.id.btn3:
+                        clickado = Integer.parseInt(btn1.getText().toString());
+                        if (clickado == (dados.get(0))) {
+                            builder.setMessage("Acertou").setTitle("Questão " + contador);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                            acertos++;
+                        } else {
+                            builder.setMessage("Errou, a resposta certa era " + (dados.get(0) + 1)).setTitle("Questão " + contador);
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        }
+                        clickado = 0;
+                        break;
+                }
+                montaTela();
+            }
+         else {
+            builder.setMessage(acertos * 20 + "%").setTitle("Resultado");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    finish();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+}
