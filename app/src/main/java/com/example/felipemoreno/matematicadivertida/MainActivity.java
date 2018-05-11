c1a10e376ba1fa169c07b87bce0b750c97ef9136
felipe-moreno <felipemorenolopes@gmail.com>
2018-05-10 21:56:43 -0300

    Ibagens

--- app/src/main/java/com/example/felipemoreno/matematicadivertida/MainActivity.java
+++ app/src/main/java/com/example/felipemoreno/matematicadivertida/MainActivity.java
@@ -25,4 +25,14 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, Aritmetica.class);
         startActivity(it);
     }
+
+    public void maiorNumero (View view){
+        Intent it = new Intent(this, MaiorNumero.class);
+        startActivity(it);
+    }
+
+    public void contagem (View view){
+        Intent it = new Intent(this, Contagem.class);
+        startActivity(it);
+    }
 }
